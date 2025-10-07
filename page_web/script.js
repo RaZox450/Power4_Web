07aefd85a83f08447b8c6a27459122fde0cda73c
RaZox450 <ethanschell8@gmail.com>
2025-10-07 14:14:20 +0200

    Modification Web

--- page_web/script.js
+++ page_web/script.js
@@ -1,3 +1,29 @@
-function startGame () {
-    
-}
+var canvas = document.getElementById("structure");
+  var context = canvas.getContext("2d");
+   
+   
+  //Rectangle
+  context.beginPath();
+  context.fillStyle="blue";  
+  context.lineWidth="10";  
+  context.rect(500,50,600,515);
+  context.fill();
+  context.closePath();
+   
+  
+  
+   
+  //Cercle
+  context.beginPath();
+  context.fillStyle = "white";
+   
+  
+for(i=0;i<=5;i++)
+{
+ for(j=0;j<=6;j++)
+  {
+  context.arc(545+85*j,95+85*i,35,Math.PI*2,false); 
+  context.fill();
+  context.beginPath();
+  }
+  }
