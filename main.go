e83bfcba31448750a8bb206359b8c27e28d5d554
CharlyYNOV <charly.rosetto@ynov.com>
2025-10-23 15:00:33 +0200

    ajout du système de jeu en Go

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"html/template"
 	"net/http"
 )
+
+// Structure pour la grille de jeu
+type Grille struct {
+	Cases    [6][7]int
+	JoueurActuel int
+	Pseudo1  string
+	Pseudo2  string
+}
+
+var grille Grille
 
 // Afficher page d'accueil
 func home(w http.ResponseWriter, r *http.Request) {
@@ -22,12 +33,69 @@ func home(w http.ResponseWriter, r *http.Request) {
 		Pseudo1: r.FormValue("player1"),
 		Pseudo2: r.FormValue("player2"),
 	}
+	
+	// Initialiser la grille pour une nouvelle partie
+	grille = Grille{
+		Cases:    [6][7]int{},
+		JoueurActuel: 1,
+		Pseudo1:  data.Pseudo1,
+		Pseudo2:  data.Pseudo2,
+	}
+	
 	tmpl.Execute(w, data)
 }
 
 func game(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("page_web/power4_jeu.html"))
-	tmpl.Execute(w, nil)
+	tmpl.Execute(w, grille)
+}
+
+// Fonction pour placer un pion
+func placerPion(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Méthode non autorisée", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var colonne int
+	err := json.NewDecoder(r.Body).Decode(&colonne)
+	if err != nil {
+		http.Error(w, "Erreur de décodage JSON", http.StatusBadRequest)
+		return
+	}
+
+	// Vérifier si la colonne est valide
+	if colonne < 0 || colonne >= 7 {
+		http.Error(w, "Colonne invalide", http.StatusBadRequest)
+		return
+	}
+
+	// Trouver la première case vide dans la colonne
+	ligne := -1
+	for i := 5; i >= 0; i-- {
+		if grille.Cases[i][colonne] == 0 {
+			ligne = i
+			break
+		}
+	}
+
+	if ligne == -1 {
+		http.Error(w, "Colonne pleine", http.StatusBadRequest)
+		return
+	}
+
+	// Placer le pion
+	grille.Cases[ligne][colonne] = grille.JoueurActuel
+
+	// Changer de joueur
+	if grille.JoueurActuel == 1 {
+		grille.JoueurActuel = 2
+	} else {
+		grille.JoueurActuel = 1
+	}
+
+	// Renvoyer la grille mise à jour
+	json.NewEncoder(w).Encode(grille)
 }
 
 // création du serveur
@@ -36,5 +104,6 @@ func main() {
 	http.Handle("/page_web/", http.StripPrefix("/page_web/", fs))
 	http.HandleFunc("/", home)
 	http.HandleFunc("/power4_jeu", game)
+	http.HandleFunc("/placer-pion", placerPion)
 	http.ListenAndServe(":5500", nil)
 }
